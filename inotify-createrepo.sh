c96b5f6651cf235e116e26be5c0c8b05b17bf45a
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2019-07-25 17:26:29 +0600

    Update inotify-createrepo.sh

--- inotify-createrepo.sh
+++ inotify-createrepo.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+source /etc/
+while true;
+do
+  inotifywait -mr -e create,modify,close_write,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | createrepo "${REPO}"
+  sleep 10
+done
