4cd4c35394880a309951705874c2feaad360d05a
Anton Patsev <patsev.anton@gmail.com>
2019-07-26 10:42:33 +0600

    chmod +x build.sh

--- inotify-createrepo.sh
+++ inotify-createrepo.sh
@@ -5,5 +5,5 @@ source /etc/inotify-createrepo.conf
 while true;
 do
   /usr/bin/inotifywait -mr -e create,modify,close_write,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | /usr/bin/createrepo "${REPO}"
-  sleep 10
+  sleep 2
 done
