a1e8f15b911b2c6ca02b20c009ba1acf415d30d5
Anton Patsev <patsev.anton@gmail.com>
2019-07-26 13:55:03 +0600

    while true if need_create 1 run createrepo

--- inotify-createrepo.sh
+++ inotify-createrepo.sh
@@ -2,15 +2,25 @@
 
 source /etc/inotify-createrepo.conf
 LOGFILE=/var/log/inotify-createrepo.log
+need_create="0"
 
 function monitoring() {
-    inotifywait -e create,delete,move -msrq --exclude ".repodata|.olddata|repodata" "${REPO}" | while read events 
+    inotifywait -e create,delete -msrq --exclude ".repodata|.olddata|repodata" "${REPO}" | while read events 
     do
       echo $events >> $LOGFILE
+      need_create="1"
       /usr/bin/createrepo "${REPO}"
-      sleep 10
     done
 }
+
+while true; do
+  if [ "$need_create" == "1" ];
+  then
+    /usr/bin/createrepo "${REPO}"
+    sleep 5
+  fi
+done
+
 
 echo "Start filesystem monitoring: Directory is $REPO, monitor logfile is $LOGFILE"
 monitoring $DIR  >> $LOGFILE &
