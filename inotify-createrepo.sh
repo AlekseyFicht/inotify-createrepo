f7a06a0f3b8708689bae957b08de3f091b88b69b
Anton Patsev <patsev.anton@gmail.com>
2019-07-26 13:09:12 +0600

    inotifywait -m -s -t 2 -r -e

--- inotify-createrepo.sh
+++ inotify-createrepo.sh
@@ -3,5 +3,5 @@
 source /etc/inotify-createrepo.conf
 
 while true; do
-  inotifywait  -s -t 2 -r -e modify,close_write,move,create,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | /usr/bin/createrepo "${REPO}"
+  inotifywait -m -s -t 2 -r -e close_write,move,create,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | /usr/bin/createrepo "${REPO}"
 done
