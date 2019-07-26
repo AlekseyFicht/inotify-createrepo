656347a65ed95a527eea0bbad29f9961c3e79903
Anton Patsev <patsev.anton@gmail.com>
2019-07-26 13:12:29 +0600

    inotifywait -m -s -r -e

--- inotify-createrepo.sh
+++ inotify-createrepo.sh
@@ -3,5 +3,5 @@
 source /etc/inotify-createrepo.conf
 
 while true; do
-  inotifywait -m -s -t 2 -r -e close_write,move,create,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | /usr/bin/createrepo "${REPO}"
+  inotifywait -m -s -r -e close_write,move,create,delete --exclude ".repodata|.olddata|repodata" "${REPO}" | /usr/bin/createrepo "${REPO}"
 done
--- inotify-createrepo.spec
+++ inotify-createrepo.spec
@@ -2,7 +2,7 @@
 
 Name:    inotify-createrepo
 Version: 0.4
-Release: 8
+Release: 9
 Summary: Createrepo backend daemon based on inotifywait
 Group:   Development Tools
 License: ASL 2.0
